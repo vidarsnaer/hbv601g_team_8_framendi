a4f84b9832ce41531d9bc9add150052b257147a1
au602716 <au602716@uni.au.dk>
2024-03-22 16:51:46 +0100

    chats are now loaded from the database, user can send message to database

--- app/src/main/java/com/example/hbv601g_t8/ChatOverviewActivity.kt
+++ app/src/main/java/com/example/hbv601g_t8/ChatOverviewActivity.kt
@@ -5,17 +5,34 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import io.github.jan.supabase.postgrest.from
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import java.time.ZonedDateTime
 
 class ChatOverviewActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
 
+    private lateinit var conversations : List<Conversation>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_overview)
 
+        runBlocking {
+            getConversationsFromDatabase()
+        }
+
         recyclerView = findViewById<RecyclerView>(R.id.chatOverviewRecyclerView).apply {
             layoutManager = LinearLayoutManager(this@ChatOverviewActivity)
+            adapter = ChatOverviewAdapter(conversations) {conversationid ->
+                val intent = Intent(this@ChatOverviewActivity, ChatActivity::class.java).apply {
+                    putExtra("CHAT_ID", conversationid)
+                }
+                startActivity(intent)
+            }
         }
     }
 
@@ -30,4 +47,11 @@ class ChatOverviewActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    private suspend fun getConversationsFromDatabase() {
+        withContext(Dispatchers.IO) {
+            conversations = SupabaseManager.supabase.from("conversation").select().decodeList()
+        }
+    }
+
 }
